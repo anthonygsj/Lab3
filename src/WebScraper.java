71441afaba282484b8168c5baff71e5c18927cef
Anthony <sgao24@illinois.edu>
2018-09-18 16:50:00 -0500

    Initial Commit.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,34 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        String input = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        int count = 0;
+        char[] line = input.toCharArray();
+        for(int i = 0; i < input.length(); i++) {
+            line = input[i].readLine().split("\\s");
+            count += line.length();
+        }
+        System.out.println(count);
+        System.out.println(input);
     }
 }
